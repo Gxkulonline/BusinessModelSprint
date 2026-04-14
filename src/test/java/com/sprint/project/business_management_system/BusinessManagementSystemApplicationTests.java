3ed0b6dcd1fbe8fb91d59b002f9db0e4912a9c42
gokul <gokul12@gmail.com>
2026-04-14 13:35:32 +0530

    Initial project setup

---
+++ src/test/java/com/sprint/project/business_management_system/BusinessManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sprint.project.business_management_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BusinessManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
