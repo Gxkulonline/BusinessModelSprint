81a912386397df8aa683b39c9f2a1e278e5f024b
Yuva <yuva@gmail.com>
2026-04-15 12:47:14 +0530

    customer

---
+++ src/main/java/com/sprint/project/business_management_system/repository/CustomerRepository.java
@@ -0,0 +1,7 @@
+package com.sprint.project.business_management_system.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.sprint.project.business_management_system.Entity.Customer;
+
+public interface CustomerRepository extends JpaRepository<Customer, Integer> {
+}
