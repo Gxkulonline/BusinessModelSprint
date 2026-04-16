0b6465e0b360a9b22c85f9bc3b07fd60bd949f09
Yuva <yuva@gmail.com>
2026-04-16 09:07:27 +0530

    Added Duplicate and Business Exception

---
+++ src/main/java/com/sprint/project/business_management_system/exception/BusinessException.java
@@ -0,0 +1,8 @@
+package com.sprint.project.business_management_system.exception;
+
+public class BusinessException extends RuntimeException{
+	public BusinessException(final String message) {
+        super(message);
+    }
+
+}
