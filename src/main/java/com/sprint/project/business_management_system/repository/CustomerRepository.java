866675e11656645120496e20a5a6d67e89f40439
Yuva <yuva@gmail.com>
2026-04-20 08:09:23 +0530

    error debug

--- src/main/java/com/sprint/project/business_management_system/repository/CustomerRepository.java
+++ src/main/java/com/sprint/project/business_management_system/repository/CustomerRepository.java
@@ -3,12 +3,13 @@ package com.sprint.project.business_management_system.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.sprint.project.business_management_system.Entity.Customer;
 
 public interface CustomerRepository extends JpaRepository<Customer, Integer> {
     List<Customer> findByCountry(String country);
-
+    @Query("SELECT p FROM Product p WHERE p.buyPrice > :price")
     List<Customer> findAllByOrderByCreditLimitDesc();
 
 }
