492fa54b63d2db8fffd18d52de8a128eda69a1ca
gokul <gokul12@gmail.com>
2026-04-21 13:36:15 +0530

    final backend

--- backend/src/main/java/com/sprint/project/business_management_system/BusinessManagementSystemApplication.java
+++ backend/src/main/java/com/sprint/project/business_management_system/BusinessManagementSystemApplication.java
@@ -7,6 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BusinessManagementSystemApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(BusinessManagementSystemApplication.class, args);
+		
+		
 	}
 }
  
